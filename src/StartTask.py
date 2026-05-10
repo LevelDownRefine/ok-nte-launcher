14a4e90287073bc27f7811b13372dc68446b02aa
winsanyu <973530837@qq.com>
2026-05-10 12:45:43 +0800

    support launcher update

--- src/StartTask.py
+++ src/StartTask.py
@@ -10,9 +10,19 @@ class StartTask(BaseTask):
         self.description = "通过启动器启动游戏"
         self.icon = FluentIcon.SYNC
 
-    def run(self):
-        self.log_info('启动器任务开始运行!', notify=True)
-        if self.wait_click_ocr(match=re.compile("开始")):
-            self.log_info('启动器任务运行完成!', notify=True)
+    def _start(self, time_out=10):
+        '''点击开始'''
+        if self.wait_click_ocr(match=re.compile("开始"), time_out=time_out):
+            self.log_info('启动器点击开始', notify=True)
         else:
-            self.log_error('启动器任务运行失败!', notify=True)
+            self.log_error('启动器点击开始失败', notify=True)
+
+    def run(self):
+        '''检测更新 -> 点击开始'''
+        self.log_info('启动器任务开始运行', notify=True)
+        if self.wait_click_ocr(match=re.compile("更新"), after_sleep=60):
+            self.log_info('启动器更新', notify=True)
+            self._start()
+        else:
+            self.log_info('未找到更新', notify=True)
+            self._start()
